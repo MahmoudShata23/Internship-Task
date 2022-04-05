83801c8420f9b46eacbdc726ed62f06039f87451
Mahmoud Shata <83148301+MahmoudShata23@users.noreply.github.com>
2022-04-06 01:51:38 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,6 +10,18 @@ import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { DetailsComponent } from './product/details/details.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+
+import { AccordionModule } from 'primeng/accordion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DropdownModule } from 'primeng/dropdown';
+import { CheckboxModule } from 'primeng/checkbox';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbPaginationModule,
+  NgbAlertModule,
+} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -18,13 +31,23 @@ import { NotFoundComponent } from './not-found/not-found.component';
     HomeComponent,
     ProductComponent,
     DetailsComponent,
-    NotFoundComponent
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    DropdownModule,
+    NgbModule,
+    NgbPaginationModule,
+    NgbAlertModule,
+    RouterModule,
+    CheckboxModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
